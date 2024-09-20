46cc671db1c41a3cabbb82e46786bb8e8e07f816
AryaShetty08 <arya.shetty08@gmail.com>
2024-09-20 00:28:30 -0400

    prob 2 fix

--- Assignment1/aas445-dl1023/component_2.py
+++ Assignment1/aas445-dl1023/component_2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
+from component_1 import check_SOn
 
 def random_rotation_matrix(naive):
      """
@@ -12,39 +13,54 @@ def random_rotation_matrix(naive):
     Returns:
     - matrix: Returns valid, randomly generated rotation matrix 
     """
-    
-     alpha = np.degrees(np.random.uniform(0, 2*np.pi))
-     beta = np.degrees(np.random.uniform(0, 2*np.pi))
-     gamma = np.degrees(np.random.uniform(0, 2*np.pi))
+     if naive:
+         alpha = np.random.uniform(0, 2*np.pi)
+         beta = np.random.uniform(0, 2*np.pi)
+         gamma = np.random.uniform(0, 2*np.pi)
 
-     testVector =  [0, 0, 1]
-     x, y, z = testVector
-     #zip(*vectors)
+         rotationX = [[1, 0, 0], [0, np.cos(alpha), -np.sin(alpha)], [0, np.sin(alpha), np.cos(alpha)]]
+         rotationY = [[np.cos(beta), 0, np.sin(beta)], [0, 1, 0], [-np.sin(beta), 0, np.cos(beta)]]
+         rotationZ = [[np.cos(gamma), -np.sin(gamma), 0], [np.sin(gamma), np.cos(gamma), 0], [0, 0, 1]]
 
-     #if naive:
-     rotationX = [[1, 0, 0], [0, np.cos(alpha), -np.sin(alpha)], [0, np.sin(alpha), np.cos(alpha)]]
-     rotationY = [[np.cos(beta), 0, np.sin(beta)], [0, 1, 0], [-np.sin(beta), 0, np.cos(beta)]]
-     rotationZ = [[np.cos(gamma), -np.sin(gamma), 0], [np.sin(gamma), np.cos(gamma), 0], [0, 0, 1]]
+         m = np.matmul(rotationX, rotationY)
+         m = np.matmul(m, rotationZ)
 
-     m = np.matmul(rotationX, rotationY)
-     m = np.matmul(m, rotationZ)
+         #print(check_SOn(m))
+         return m
+            
+     else:
+         m = np.zeros((3, 3))
+         while not check_SOn(m):
+            x1, x2, x3 = np.random.uniform(0, 1, 3)
 
+            v = [np.cos(2*np.pi*x2)*np.sqrt(x3), np.sin(2*np.pi*x2)*np.sqrt(x3), np.sqrt(1-x3)]
+            v = np.reshape(v, (3,1))
 
-          
-     #else:
+            mReflection = 2*np.dot(v,np.transpose(v)) - np.eye(3)
+            rotationZ = [[np.cos(2*np.pi*x1), -np.sin(2*np.pi*x1), 0], [np.sin(2*np.pi*x1), np.cos(2*np.pi*x1), 0], [0, 0, 1]]
 
-
-     #return m
+            m = np.dot(mReflection, rotationZ)
+         #print(check_SOn(m))
+         return m
 
 if __name__ == "__main__":
-     testVectors =  [[0, 0, 1], [0, 0, 2]]
-     x, y, z = zip(*testVectors)
      fig = plt.figure()
      ax = fig.add_subplot(111, projection='3d')
-     #ax.set_xlim([-1,1])
-     ax.quiver(0, 0, 0, x, y, z, color='b')
+
+     for i in range(2000):
+         rotationMatrix = random_rotation_matrix(True)
+         startVector = [0, 1, 0]
+         v = [0.1, 0, 0]
+         rotatedStart = np.matmul(rotationMatrix, startVector)
+         rotated_v = np.matmul(rotationMatrix, v)
+         #ax.quiver(startVector[0], startVector[1], startVector[2], v[0], v[1], v[2], color='b')
+         ax.quiver(rotatedStart[0], rotatedStart[1], rotatedStart[2], rotated_v[0], rotated_v[1], rotated_v[2], color='b')
+     
      ax.set_xlabel('X')
      ax.set_ylabel('Y')
      ax.set_zlabel('Z')
+     ax.set_xlim([-1, 1])
+     ax.set_ylim([-1, 1])
+     ax.set_zlim([-1, 1])
      ax.set_title('Random Rotation Visualization')
      plt.show()
