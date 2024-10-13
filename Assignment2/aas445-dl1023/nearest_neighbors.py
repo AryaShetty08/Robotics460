333cbd7e80cf13cad61b24f663dc29370d841667
AryaShetty08 <arya.shetty08@gmail.com>
2024-10-13 14:21:33 -0400

    stuff for visual

--- Assignment2/aas445-dl1023/nearest_neighbors.py
+++ Assignment2/aas445-dl1023/nearest_neighbors.py
@@ -20,6 +20,8 @@ def nearest_neighbors(robot, target, k, configs):
     """
 
     #traget is a tuple??
+    
+
     configurations = []
     with open(configs, "r") as file:
         for line in file:
@@ -34,8 +36,10 @@ def nearest_neighbors(robot, target, k, configs):
 
 
     elif robot == "freeBody":
-         for i in range(len(configurations)):
-            distance = math.sqrt(math.pow((target[0] - configurations[i][0]), 2) + math.pow((target[1] - configurations[i][1]), 2) + math.pow((target[2] - configurations[i][2])))
+        #ax.set_zlim(0, 20)
+        #ax.set_zlabel('Z')
+        for i in range(len(configurations)):
+            distance = math.sqrt(math.pow((target[0] - configurations[i][0]), 2) + math.pow((target[1] - configurations[i][1]), 2) + math.pow((target[2] - configurations[i][2]), 2))
             nearestConfigs.append((distance, i))
     
     sortedConfigs = sorted(nearestConfigs, key=lambda x: x[0])
@@ -44,12 +48,33 @@ def nearest_neighbors(robot, target, k, configs):
         nearestConfigs.append(configurations[sortedConfigs[i][1]])
 
     # visualize the stuff for nearestConfigs + target on plot
+    
+    if robot == "arm":
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(bottom=0.2)
+        ax.set_xlim(0, 20)
+        ax.set_ylim(0, 20)
+
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        x_values, y_values = zip(*nearestConfigs)
+        ax.scatter(x_values, y_values)
+        ax.grid(True) 
+    elif robot == "freeBody":
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        x_values, y_values, z_values = zip(*nearestConfigs)
+        ax.scatter(x_values, y_values, z_values)  
+
+    ax.set_title('KNN Visualization')
+    plt.show()
+
     return nearestConfigs
 
 if __name__ == "__main__":
-    testRobot = "arm"
-    testTarget = (0, 0)
+    testRobot = "freeBody"
+    testTarget = (0, 0, 0)
     k = 2
     testFile = "configs.txt"
-    
+
     print(nearest_neighbors(testRobot, testTarget, k, testFile))
