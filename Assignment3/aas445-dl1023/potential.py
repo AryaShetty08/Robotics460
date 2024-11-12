c1c2abb8802bc77be6c90efb8b196a718dc97028
AryaShetty08 <arya.shetty08@gmail.com>
2024-11-11 20:46:13 -0500

    more pot stuff

--- Assignment3/aas445-dl1023/potential.py
+++ Assignment3/aas445-dl1023/potential.py
@@ -12,6 +12,28 @@ import time
 # calculate and use gradient to find force for robot to move this is in 2d no rotation 
 # and implement
 
+# this one is for goal
+def attractive_potential(robot_pose, goal):
+    # they used a quadratic here??
+    att_pot = 0.5 * ((robot_pose[0] - goal[0])**2 + (robot_pose[1] - goal[1])**2)
+    return att_pot
+
+# for the obstacles
+def repulsive_potential(robot_pose, obs):
+    # they used inverse function?
+    dist = max(0.01, ((robot_pose[0] - obs[0])**2 + (robot_pose[1] - obs[1])**2))
+    return
+
+def total_potential(robot_pose, goal, obstacles):
+    u_att = attractive_potential(robot_pose, goal)
+    u_rep = 0
+    for obs in obstacles:
+        u_rep += repulsive_potential(robot_pose, obs)
+    return u_att + u_rep
+
+# gradient
+
+
 def main():
     print("hello")
     parser = argparse.ArgumentParser(description="Potential Function")
